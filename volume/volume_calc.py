37f264d61b932f8e2c279f9ca0e288a3c189dd62
Piotr Kowalczyk <56620@student.ue.poznan.pl>
2024-04-06 10:06:51 +0200

    dodano objetosci

---
+++ volume/volume_calc.py
@@ -0,0 +1,10 @@
+import math
+
+def objetosc_szescianu(krawedz):
+    return krawedz ** 3
+
+def objetosc_walca(promien, wysokosc):
+    return math.pi*promien*2*wysokosc
+
+def objetosc_kuli(promien):
+    return 4/3*math.pi*promien**3
