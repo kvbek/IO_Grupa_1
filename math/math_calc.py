c25ef464a89ca71762389b8450bb27c1e5b309e9
Jakub Marciniak <jakub.s.marciniak@gmail.com>
2024-04-06 10:29:22 +0200

    Dodaje zadanie 6

---
+++ math/math_calc.py
@@ -0,0 +1,14 @@
+def hours_to_min(hours: float):
+    try:
+        return float(hours) * 60
+    except ValueError:
+        print("Error: Parameter is not the correct type") 
+
+def inch_to_cm(inch: float):
+    try:
+        return inch * 2.54
+    except ValueError:
+        print("Error: Parameter is not the correct type")
+
+def km_to_mile(km: float):
+    return km * '1.60934'
