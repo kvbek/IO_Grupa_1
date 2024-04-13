c1f8c35ad8c5982fd83d0d15b1d64f9032ebbb9c
Julia <93770@student.ue.poznan.pl>
2024-04-13 11:24:38 +0200

    poprawiłam pole trapezu

--- area/area_calc.py
+++ area/area_calc.py
@@ -7,7 +7,7 @@ def pole_trojkata(podstawa, wysokosc):
     return podstawa*wysokosc/2
 
 def pole_trapezu(podstawa1, podstawa2, wysokosc):
-    return podstawa1+podstawa2*wysokosc/2
+    return (podstawa1+podstawa2)*wysokosc/2
 
 def pole_kola(promien):
     return math.pi*promien**2
