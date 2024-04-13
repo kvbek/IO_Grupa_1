69647204c0fde35615f65314b4ccd9b0e0703074
Julia <93770@student.ue.poznan.pl>
2024-04-13 11:47:49 +0200

    dodałam wywołania funkcji

--- volume/volume_calc.py
+++ volume/volume_calc.py
@@ -8,3 +8,12 @@ def objetosc_walca(promien, wysokosc):
 
 def objetosc_kuli(promien):
     return 4/3*math.pi*promien**3
+
+szescian_krawedz = int(input("Podaj długość krawędzi sześcianu: "))
+walec_dl = int(input("Podaj długość promienia walca: "))
+walec_wys = int(input("Podaj wysokość walca: "))
+kula_promien = int(input("Podaj długość promienia kuli: "))
+
+print(f"Objętość sześcianu wynosi {objetosc_szescianu(szescian_krawedz)}.\n" 
++ f"Objętość walca wynosi {objetosc_walca(walec_dl, walec_wys)}.\n"
++ f"Objętość kuli wynosi {objetosc_kuli(kula_promien)}.")
